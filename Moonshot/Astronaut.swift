0aab37e8b4bac8040013b7989ecb627dc19db9f6
Eugene Cross <eugene.cross@gmail.com>
2023-08-22 10:35:05 +0100

    Project complete, prior to modifications

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Eugene on 21/08/2023.
+//
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
