419ae362e9e4c13037984b498465d785ea9b3a90
Eugene Cross <eugene.cross@gmail.com>
2023-08-26 12:42:03 +0100

    Challenge 3, split into grid and list views with button

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,54 +10,104 @@ import SwiftUI
 struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    @State private var showList = false
+    
+    var body: some View {
+        NavigationView {
+            Group {
+                if showList {
+                    ListLayoutView(missions: missions)
+                } else {
+                    GridLayoutView(missions: missions)
+                }
+            }
+            .navigationTitle("MoonShot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .toolbar {
+                Button("\(showList ? "Grid": "List")") {
+                    showList.toggle()
+                }
+            }
+        }
+    }
+}
+
+struct GridLayoutView: View {
+    
+    let missions: [Mission]
+    
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                        
                             
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formatterLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formatterLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
                         }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
                     }
                 }
-                .padding([.horizontal, .bottom])
             }
-            .navigationTitle("MoonShot")
-            .background(.darkBackground)
-            .preferredColorScheme(.dark)
+            .padding([.horizontal, .bottom])
         }
-       
-            
+    }
+    
+}
+
+struct ListLayoutView: View {
+    
+    let missions: [Mission]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission)
+                } label: {
+                    HStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                        
+                        Text(mission.displayName)
+                            .padding(.leading)
+
+                    }
+                    
+                }
+            }
+            .listStyle(.plain)
+            .listRowBackground(Color.darkBackground)
+        }
     }
 }
 
